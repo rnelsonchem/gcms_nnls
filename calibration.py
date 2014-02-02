ec7ecf918518f7bdf4967ad1248341db98110b8f
Ryan Nelson <rnelsonchem@gmail.com>
2014-02-02 09:09:39 -0500

    Added command args for bkg to cal script
    The calibration automation script was modified to accept command line
    arguements. The first argument '--nobkg' turns off the useage of a MS slice as
    a background for fitting. The second argument, '--bkg_time', sets the time
    for the background MS slice. The values of these two arguments is saved into
    the hdf5 calibration file as attributes of the 'cals' table. That way they can
    be queried at a later time.

--- calibration.py
+++ calibration.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 
 import numpy as np
 import tables as pyt
@@ -15,6 +16,22 @@ h5f_name = 'cal.h5'
 
 ##############################
 
+# Get command line values
+parser = argparse.ArgumentParser()
+
+# This is a little backwards. If you request no background, this argument gets
+# set to False. That is because of the call to ref_build later.
+parser.add_argument('--nobkg', action='store_const', default=True, const=False,
+        help='Turn off the usage of a single  MS slice as a background in \
+                fitting.')
+
+parser.add_argument('--bkg_time', default=0.0, type=float, 
+        help='The time position of the spectrum to use as a background for \
+        fitting. This has no effect if "--nobkg" is used.' )
+
+args = parser.parse_args()
+
+
 class CalTable( pyt.IsDescription ):
     cpd = pyt.StringCol( 50, pos=0 )
     int_start = pyt.Float64Col( pos=1 )
@@ -30,6 +47,9 @@ refs = gcms.refs_file( ref_name )
 
 h5f = pyt.openFile(h5f_name, 'w', 'GCMS Calibrations')
 table = h5f.createTable('/', 'cals', CalTable, )
+# Here I'm storing the background information in case I need to know later.
+table.attrs.bkg = args.nobkg
+table.attrs.bkg_time = args.bkg_time
 
 for n, ref in enumerate(refs):
     name = ref[:-4]
@@ -62,8 +82,10 @@ for n, ref in enumerate(refs):
     plt.figure()
     for f in files:
         aia = gcms.AIAFile( os.path.join(name, f) )
-        aia.ref_build( refs )
+
+        aia.ref_build(refs, bkg=args.nobkg, bkg_time=args.bkg_time)
         aia.nnls()
+
         integral = aia.integrate(start, stop)
         ints.append( integral[n] )
         aias.append( aia )
