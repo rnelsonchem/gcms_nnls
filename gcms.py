4dbfe7805209bead9bc2e3318ae1786b90f85881
Ryan Nelson <rnelsonchem@gmail.com>
2014-06-14 20:45:47 -0400

    Added a retention time filter to nnls function
    I needed a way to filter potential peak fitting to a selection of compounds
    based on their known experimental retention times. I added some code to the
    nnls function in my AIAFile object to do just that. It requires that the
    reference file has retention time information included with the reference
    spectra.
    
    Seems to work okay on several tests.

--- gcms.py
+++ gcms.py
@@ -245,11 +245,49 @@ class AIAFile(object):
         self.ref_array = np.array(self.ref_array)
 
 
-    def nnls(self, ):
+    def nnls(self, rt_filter=False, rt_win=0.2, rt_adj=0.):
         fits = []
+        
+        # If a retention time filter is requested, then build up an array of
+        # retention times from the meta data
+        if rt_filter == True:
+            rts = []
+            for name in self.ref_files:
+                if name == 'Background':
+                    rts.append( -5. )
+                    continue
+                rt = self.ref_meta[name]['RT']
+                rts.append(rt)
+            self.ret_times = np.array(rts, dtype=float)
 
-        for ms in self.intensity:
-            fit, junk = spo.nnls(self.ref_array.T, ms)
+        for time, ms in zip(self.times, self.intensity):
+            # If no retention time filter, just do standard fit
+            if rt_filter == False:
+                fit, junk = spo.nnls(self.ref_array.T, ms)
+
+            else:
+                # Create a boolean RT filter mask
+                mask = ((self.ret_times + rt_adj) > (time - rt_win)) & \
+                        ((self.ret_times + rt_adj) < (time + rt_win))
+                zeros = np.zeros( len(self.ref_files) )
+                # Check if the filter has any hits
+                msum = mask.sum()
+                if msum != 0:
+                    if self.ref_files[-1] == 'Background':
+                        mask[-1] = True
+                    ref_arr = self.ref_array[mask]
+                    fit, junk = spo.nnls(ref_arr.T, ms)
+                    zeros[mask] = fit
+                    fit = zeros
+                # If no RT hits, fit the data with either the background or
+                # use only zeros
+                else:
+                    if self.ref_files[-1] == 'Background':
+                        fit, junk = spo.nnls(self.ref_array[-1].reshape(-1,1)
+                                , ms)
+                        zeros[-1] = fit[0]
+                    fit = zeros
+
             fits.append( fit )
 
         self.fits = np.array( fits )
